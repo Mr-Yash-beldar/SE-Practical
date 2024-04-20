cb4b77aa5838ad9ef403390b5d4bf1724671dd4d
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-04-20 15:02:51 +0530

    updated version

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const names="Yashodip Beldar"
 
 console.log(names);
+
+function displayName() {
+    var name = "John Doe"; // Replace "John Doe" with the desired name
+    alert("Your name is: " + name);
+}
