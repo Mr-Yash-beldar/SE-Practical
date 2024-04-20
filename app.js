b60a881575395d5b0f559d4d61abf63d26fd77b2
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-04-20 15:05:59 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
+    
 const names="Yashodip Beldar"
-
-console.log(names);
-
 function displayName() {
-    var name = "John Doe"; // Replace "John Doe" with the desired name
-    alert("Your name is: " + name);
+    alert("Your name is: " + names);
 }
